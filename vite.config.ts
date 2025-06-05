c88c21928348c2bf1a18b6fc71a7243707b43892
David <david_yanez99@outlook.es>
2025-06-04 22:25:07 -0500

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
   
   return {
     plugins: [react()],
-    base: isProduction ? '/genhora-front/' : '/',
+    base: isProduction ? 'https://francisdayl.github.io/genhora-front' : '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
