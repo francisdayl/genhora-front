e4ed1720e1118b5ddcba00104ca73f889e571293
David Yánez <73242585+francisdayl@users.noreply.github.com>
2025-06-04 22:09:05 -0500

    add deploy workflow (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'cypress.*', 'cypress/*'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'cypress.*', 'cypress/*','vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react() ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@hooks': path.resolve(__dirname, './src/hooks')
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === 'production'
+  
+  return {
+    plugins: [react()],
+    base: isProduction ? '/genhora-front/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@lib': path.resolve(__dirname, './src/lib'),
+        '@hooks': path.resolve(__dirname, './src/hooks')
+      },
     },
-  },
+  }
 })
